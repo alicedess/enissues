b8bf637336096b25522e3fa128db962edc038970
alicedess <alicedgg@gmail.com>
2025-01-28 11:55:54 +0100

    view and script form first draft

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-console.log("coucou");
+const express = require("express");
 
-console.log("coucou branche 1");
+const app = express();
+const port = 3000;
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+let issue = [];
+
+app.get("/", (req, res) => {
+  
+  res.render("index", { issue });
+});
+
+app.post("/issues/create", (req, res) => {
+
+  const { issue, name } = req.body;
+
+  issues.push({ issue, name });
+  res.redirect("/");
+});
+app.listen(port, () => {
+  console.log("Le serveur tourne sur le port " + port);
+});
